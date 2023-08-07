079699baacdcb4b3f6f6ec4e6870305263e3db5c
paulg44 <paulgarton1984@hotmail.com>
2023-08-07 18:27:19 +0100

    add one file, having issues with react router dom

---
+++ my-app/src/SiteDescriptionCard/SiteDescriptionCard.tsx
@@ -0,0 +1,14 @@
+// Commponent for site description card
+
+function SiteDescriptionCard() {
+  return (
+    <div>
+      <h2>Some Site</h2>
+      <p>Some site description</p>
+      <p>IMAGE</p>
+      {/* Hard coded link for the moment */}
+    </div>
+  );
+}
+
+export default SiteDescriptionCard;
